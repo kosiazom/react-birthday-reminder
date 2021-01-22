e0fc9af1915408f75755f9fce9d74fb2a99b89f4
kosiazom <kosiazom@gmail.com>
2021-01-21 22:42:07 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+const App = () => {
+  return <main>
+   <section className="container"></section>
+    </main>;
+}
+
+export default App;
